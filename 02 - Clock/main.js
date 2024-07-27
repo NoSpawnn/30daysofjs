9e067596cee613ad8e9d5f2a4a97190bb4c64abd
NoSpawnn <adam.blighe@protonmail.com>
2024-07-27 15:46:37 +0100

    Refactor, fix deg calculations

--- 02 - Clock/main.js
+++ 02 - Clock/main.js
@@ -2,18 +2,26 @@ const hoursHand = document.querySelector(".hour-hand");
 const minutesHand = document.querySelector(".min-hand");
 const secondsHand = document.querySelector(".second-hand");
 
+const rotate = (elem, degrees) => {
+  elem.style.transform = `rotate(${degrees}deg)`;
+};
+
 function setDate() {
   const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  const hoursDeg = (hours / 12) * 360 + 90;
-  const minutesDeg = (minutes / 60) * 360 + 90;
-  const secondsDeg = (seconds / 60) * 360 + 90;
+  const second = now.getSeconds();
+  const secondsDeg = (second / 60) * 360 + 90;
+  rotate(secondsHand, secondsDeg);
 
-  hoursHand.style.transform = `rotate(${hoursDeg}deg)`;
-  minutesHand.style.transform = `rotate(${minutesDeg}deg)`;
-  secondsHand.style.transform = `rotate(${secondsDeg}deg)`;
+  const minute = now.getMinutes();
+  const minutesDeg = (minute / 60) * 360 + (second / 60) * 6 + 90;
+  rotate(minutesHand, minutesDeg);
+
+  const hour = now.getHours();
+  const hoursDeg = (hour / 12) * 360 + (minute / 60) * 30 + 90;
+  rotate(hoursHand, hoursDeg);
 }
 
 setInterval(setDate, 1000);
+
+// Call immediately on page load
+setDate();
