7f5a09eb6b414f43a70d06af47d93f18a511bd7f
NoSpawnn <adam.blighe@protonmail.com>
2024-07-29 16:58:17 +0100

    Day 23 done

---
+++ 23 - Speech Synthesis/main.js
@@ -0,0 +1,40 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+
+msg.text = document.querySelector("[name=text]").value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter((voice) => voice.lang.includes("en"))
+    .map(
+      (voice) =>
+        `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
+    )
+    .join("");
+}
+
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === this.value);
+}
+
+function toggle(restart = true) {
+  speechSynthesis.cancel();
+
+  if (restart) speechSynthesis.speak(msg);
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
+options.forEach((option) => option.addEventListener("change", setOption));
